76a38a24f8a4d2b902b47d0ea72f74ab9ec18f3d
Dennis Leon <leonde@vmware.com>
2021-05-03 13:54:07 -0700

    Refactor
    - Make Factory and Factory instance consistent in naming (LibraryLoader renamed to LibraryExecution)
    - Make ParseYaml consistent with other Eval functions naming
    
    Authored-by: Dennis Leon <leonde@vmware.com>

--- pkg/cmd/template/cmd.go
+++ pkg/cmd/template/cmd.go
@@ -124,14 +124,14 @@ func (o *Options) RunWithFiles(in Input, ui ui.UI) Output {
 	})
 
 	libraryCtx := workspace.LibraryExecutionContext{Current: rootLibrary, Root: rootLibrary}
-	rootLibraryLoader := libraryExecutionFactory.New(libraryCtx)
+	rootLibraryExecution := libraryExecutionFactory.New(libraryCtx)
 
-	schema, librarySchemas, err := rootLibraryLoader.Schemas(nil)
+	schema, librarySchemas, err := rootLibraryExecution.Schemas(nil)
 	if err != nil {
 		return Output{Err: err}
 	}
 
-	values, libraryValues, err := rootLibraryLoader.Values(valuesOverlays, schema)
+	values, libraryValues, err := rootLibraryExecution.Values(valuesOverlays, schema)
 	if err != nil {
 		return Output{Err: err}
 	}
@@ -146,7 +146,7 @@ func (o *Options) RunWithFiles(in Input, ui ui.UI) Output {
 		}
 	}
 
-	result, err := rootLibraryLoader.Eval(values, libraryValues, librarySchemas)
+	result, err := rootLibraryExecution.Eval(values, libraryValues, librarySchemas)
 	if err != nil {
 		return Output{Err: err}
 	}
